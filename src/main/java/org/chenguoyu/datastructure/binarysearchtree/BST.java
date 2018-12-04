6ea68aefb7e5559b996ece7efd0386c42d160afa
陈国钰 <chenguoyu_sir@163.com>
2018-12-04 10:28:47 +0800

    添加二分搜索树删除

--- src/main/java/org/chenguoyu/datastructure/binarysearchtree/BST.java
+++ src/main/java/org/chenguoyu/datastructure/binarysearchtree/BST.java
@@ -1,5 +1,7 @@
 package org.chenguoyu.datastructure.binarysearchtree;
 
+import org.chenguoyu.datastructure.queue.LinkedListQueue;
+import org.chenguoyu.datastructure.queue.Queue;
 import org.chenguoyu.datastructure.stack.LinkedListStack;
 import org.chenguoyu.datastructure.stack.Stack;
 
@@ -11,6 +13,9 @@ import org.chenguoyu.datastructure.stack.Stack;
  * @program DataStructures
  */
 public class BST<T extends Comparable<T>> {
+    /**
+     * 二分搜索树的节点信息
+     */
     private class Node {
         T value;
         Node left, right;
@@ -30,7 +35,13 @@ public class BST<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * 根节点
+     */
     private Node root;
+    /**
+     * 树中的元素数量
+     */
     private int size;
 
     public BST() {
@@ -38,14 +49,29 @@ public class BST<T extends Comparable<T>> {
         size = 0;
     }
 
-    public int getSize() {
+    /**
+     * 返回元素数量
+     *
+     * @return
+     */
+    public int size() {
         return size;
     }
 
+    /**
+     * 判断树是否为空
+     *
+     * @return
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * 添加元素
+     *
+     * @param e
+     */
     public void add(T e) {
         root = add(root, e);
     }
@@ -55,64 +81,85 @@ public class BST<T extends Comparable<T>> {
             size++;
             return new Node(e);
         }
-        if (root.value.compareTo(e) < 0) {
+        if (root.value.compareTo(e) > 0) {
             root.left = add(root.left, e);
-        } else if (root.value.compareTo(e) > 0) {
+        } else if (root.value.compareTo(e) < 0) {
             root.right = add(root.right, e);
         }
         return root;
     }
 
+    /**
+     * 树中是否包含元素
+     *
+     * @param e
+     * @return
+     */
     public boolean contain(T e) {
         return contain(root, e);
     }
 
-    public boolean contain(Node root, T e) {
+    private boolean contain(Node root, T e) {
         if (root == null) {
             return false;
         }
-        if (root.value.equals(e)) {
-            return true;
-        } else if (root.value.compareTo(e) < 0) {
+        if (root.value.compareTo(e) > 0) {
             return contain(root.left, e);
-        } else {
+        } else if (root.value.compareTo(e) < 0) {
             return contain(root.right, e);
+        } else {
+            return true;
         }
-    }
-
-
-    public void preOrder() {
-        preOrder(root);
     }
 
     /**
      * 前序遍历
-     *
-     * @param root
      */
+    public void preOrder() {
+        preOrder(root);
+    }
+
     private void preOrder(Node root) {
         if (root == null) {
             return;
         }
-        System.out.println(root.value);
+        System.out.print(root.value + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
     /**
-     * 非递归的前序遍历
+     * 非递归的前序遍历 使用栈
      */
     public void preOrderNR() {
         Stack<Node> stack = new LinkedListStack<>();
         stack.push(root);
         while (!stack.isEmpty()) {
             Node node = stack.pop();
-            System.out.println(node.value);
+            System.out.print(node.value + " ");
             if (node.left != null) {
                 stack.push(node.left);
             }
             if (node.right != null) {
                 stack.push(node.right);
+            }
+        }
+    }
+
+    /**
+     * 二分搜索树的层序遍历
+     */
+    public void levelOrder() {
+        Queue<Node> queue = new LinkedListQueue<>();
+        queue.enqueue(root);
+        while (!queue.isEmpty()) {
+            Node node = queue.dequeue();
+            System.out.print(node.value + " ");
+            if (node.left != null) {
+                queue.enqueue(node.left);
+            }
+            if (node.right != null) {
+                queue.enqueue(node.right);
             }
         }
     }
@@ -128,26 +175,166 @@ public class BST<T extends Comparable<T>> {
         if (root == null) {
             return;
         }
-        preOrder(root.left);
-        System.out.println(root.value);
-        preOrder(root.right);
+        inOrder(root.left);
+        System.out.print(root.value + " ");
+        inOrder(root.right);
     }
 
     /**
      * 后序遍历
      */
     public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node root) {
         if (root == null) {
             return;
         }
-        preOrder(root.left);
-        preOrder(root.right);
-        System.out.println(root.value);
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.value + " ");
+    }
+
+    /**
+     * 移除最小的元素
+     *
+     * @return
+     */
+    public T removeMin() {
+        T minimum = minimum();
+        root = removeMin(root);
+        return minimum;
+    }
+
+    private Node removeMin(Node root) {
+        if (root.left == null) {
+            size--;
+            return root.right;
+        }
+        root.left = removeMin(root.left);
+        return root;
+    }
+
+    /**
+     * 查找最小的元素
+     *
+     * @return
+     */
+    public T minimum() {
+        if (isEmpty()) {
+            throw new NullPointerException("BST is empty");
+        }
+        return minimum(root).value;
+    }
+
+    private Node minimum(Node root) {
+        if (root.left == null) {
+            return root;
+        }
+        return minimum(root.left);
+    }
+
+    /**
+     * 移除最大的元素
+     *
+     * @return
+     */
+    public T removeMax() {
+        T maximum = maximum();
+        root = removeMax(root);
+        return maximum;
+    }
+
+    public Node removeMax(Node root) {
+        if (root.right == null) {
+            size--;
+            return root.left;
+        }
+        root.right = removeMax(root.right);
+        return root;
+    }
+
+    /**
+     * 查找最大的元素
+     *
+     * @return
+     */
+    public T maximum() {
+        if (isEmpty()) {
+            throw new NullPointerException("二分搜索树为空");
+        }
+        return maximum(root).value;
+    }
+
+    private Node maximum(Node root) {
+        if (root.right == null) {
+            return root;
+        }
+        return maximum(root.right);
+    }
+
+    /**
+     * 移除元素
+     *
+     * @param e
+     */
+    public void remove(T e) {
+        root = remove(root, e);
+    }
+
+    private Node remove(Node root, T e) {
+        if (root == null) {
+            return null;
+        }
+        if (root.value.compareTo(e) > 0) {
+            root.left = remove(root.left, e);
+            return root;
+        } else if (root.value.compareTo(e) < 0) {
+            root.right = remove(root.right, e);
+            return root;
+        } else {
+            if (root.left == null) {
+                size--;
+                return root.right;
+            } else if (root.right == null) {
+                size--;
+                return root.left;
+            } else {
+                /*
+                 * 查找到右子树的最小的节点
+                 * 将右子树最小的节点替换到被删除的节点的位置
+                 */
+                Node minimum = minimum(root.right);
+                minimum.right = removeMin(root.right);
+                minimum.left = root.left;
+                return minimum;
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        StringBuilder sb = new StringBuilder();
+        generateString(root, 0, sb);
+        return sb.toString();
     }
 
+    private void generateString(Node root, int depth, StringBuilder result) {
+        if (root == null) {
+            result.append(generateDepthString(depth) + "NULL\n");
+            return;
+        }
+        result.append(generateDepthString(depth) + root.value + "\n");
+        generateString(root.left, depth + 1, result);
+        generateString(root.right, depth + 1, result);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            sb.append("--");
+        }
+        return sb.toString();
+    }
 }
--- src/test/java/org/chenguoyu/datastructure/binarysearchtree/BSTTest.java
+++ src/test/java/org/chenguoyu/datastructure/binarysearchtree/BSTTest.java
@@ -1,6 +1,10 @@
 package org.chenguoyu.datastructure.binarysearchtree;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * @author chenguoyu
@@ -8,24 +12,123 @@ import org.junit.Test;
  * @program DataStructures
  */
 public class BSTTest {
-    public static void main(String[] args) {
+
+    BST<Integer> bst = new BST<>();
+
+    // 打乱数组顺序
+    private static void shuffle(Object[] arr) {
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int pos = (int) (Math.random() * (i + 1));
+            Object t = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = t;
+        }
+    }
+
+    /**
+     * /////////////////
+     * //      5      //
+     * //    /   \    //
+     * //   3    6    //
+     * //  / \    \   //
+     * // 2  4     8  //
+     * /////////////////
+     */
+    @Test
+    public void testOrder() {
         BST<Integer> bst = new BST<>();
         int[] nums = {5, 3, 6, 8, 4, 2};
-        for(int num: nums)
+        for (int num : nums)
             bst.add(num);
 
-        /////////////////
-        //      5      //
-        //    /   \    //
-        //   3    6    //
-        //  / \    \   //
-        // 2  4     8  //
-        /////////////////
-//        bst.preOrder();
-        bst.inOrder();
-//        bst.postOrder();
+
+        System.out.print("二分搜索树的前序遍历");
+        bst.preOrder();
+        System.out.println();
+        System.out.print("二分搜索树的非递归前序遍历");
+        bst.preOrderNR();
         System.out.println();
 
+        System.out.print("二分搜索树的中序遍历");
+        bst.inOrder();
+        System.out.println();
+
+        System.out.print("二分搜索树的后序遍历");
+        bst.postOrder();
+        System.out.println();
+
+        System.out.print("二分搜索树的层序遍历");
+        bst.levelOrder();
+        System.out.println();
+
+
         System.out.println(bst);
+    }
+
+    @Before
+    public void createBST() {
+        Random random = new Random();
+        int n = 1000;
+        for (int i = 0; i < n; i++)
+            bst.add(random.nextInt(10000));
+    }
+
+    @Test
+    public void testRemoveMin() {
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty())
+            nums.add(bst.removeMin());
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++)
+            if (nums.get(i - 1) > nums.get(i)) {
+                throw new IllegalArgumentException("Error!");
+            }
+        System.out.println("removeMin test completed.");
+    }
+
+    @Test
+    public void testRemoveMax() {
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty())
+            nums.add(bst.removeMax());
+
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++)
+            if (nums.get(i - 1) < nums.get(i))
+                throw new IllegalArgumentException("Error!");
+        System.out.println("removeMax test completed.");
+    }
+
+    @Test
+    public void testRemove() {
+
+        BST<Integer> bst = new BST<>();
+        Random random = new Random();
+
+        int n = 1000;
+
+        for (int i = 0; i < n; i++)
+            bst.add(random.nextInt(n));
+
+        /*
+         * 注意, 由于随机生成的数据有重复, 所以bst中的数据数量大概率是小于n的
+         * order数组中存放[0...n)的所有元素
+         */
+        Integer[] order = new Integer[n];
+        for (int i = 0; i < n; i++)
+            order[i] = i;
+        // 打乱order数组的顺序
+        shuffle(order);
+
+        // 乱序删除[0...n)范围里的所有元素
+        for (int i = 0; i < n; i++)
+            if (bst.contain(order[i])) {
+                bst.remove(order[i]);
+                System.out.println("After remove " + order[i] + ", size = " + bst.size());
+            }
+
+        // 最终整个二分搜索树应该为空
+        System.out.println(bst.size());
     }
 }
