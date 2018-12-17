560c53a45085bf67844a7724b9f3f8305614a63f
陈国钰 <chenguoyu_sir@163.com>
2018-12-17 09:39:01 +0800

    红黑树的添加

--- src/main/java/org/chenguoyu/datastructure/tree/RBTree.java
+++ src/main/java/org/chenguoyu/datastructure/tree/RBTree.java
@@ -2,6 +2,12 @@ package org.chenguoyu.datastructure.tree;
 
 /**
  * 红黑树
+ * 红黑树的五个性质
+ * 1. 每个节点为红色的，或者是黑色的
+ * 2. 根节点是黑色的
+ * 3. 每个叶子节点(最后的空节点)是黑色的
+ * 4. 如果一个节点是红色的，则它的两个子节点都是黑色的
+ * 5. 对于每个节点，该节点到后代的节点所经过了黑色节点数量是相同的(黑平衡)
  *
  * @author chenguoyu
  * @date 2018-12-13
@@ -78,18 +84,103 @@ public class RBTree<K extends Comparable, V> {
         root.color = BLACK;
     }
 
-    private Node put(Node root, K key, V value) {
-        if (root == null) {
+    /*等同于在二三树的三节点的右侧添加元素 这时不需要进行右旋转，只需要进行颜色的翻转即可
+     *      node                   node
+     *     /   \     右旋转        /   \
+     *    x     Y   ------->     x     Y
+     */
+
+    /**
+     * 颜色翻转
+     * 等同于在二三树中的三节点添加一个元素，使他变成一个临时的四节点，这时需要进行颜色的翻转
+     *
+     * @param node
+     */
+    private void flipColors(Node node) {
+        node.color = RED;
+        node.left.color = BLACK;
+        node.right.color = BLACK;
+    }
+    /* x为添加的元素，为保证红色节点左倾，需要进行左旋转 并且将x的颜色与node的颜色互换
+     * 等同于像二三数中的二节点添加元素，将二节点变成三节点
+     *   node                     x
+     *  /   \     左旋转         /  \
+     * T1   x   --------->   node   T3
+     *     / \              /   \
+     *    T2 T3            T1   T2
+     */
+
+    /**
+     * 左旋转
+     *
+     * @param node
+     * @return
+     */
+    private Node leftRotate(Node node) {
+        Node x = node.right;
+
+        // 左旋转
+        node.right = x.left;
+        x.left = node;
+
+        x.color = node.color;
+        node.color = RED;
+
+        return x;
+    }
+    /* y为新添加的元素
+     * 等同于在二三树中的三节点添加一个元素，使他变成一个临时的四节点，这时需要进行又旋转，然后进行颜色的翻转
+     *      node                   x
+     *     /   \     右旋转       /  \
+     *    x    T2   ------->   y   node
+     *   / \                       /  \
+     *  y  T1                     T1  T2
+     */
+
+    /**
+     * 右旋转
+     *
+     * @param node
+     * @return
+     */
+    private Node rightRotate(Node node) {
+
+        Node x = node.left;
+
+        // 右旋转
+        node.left = x.right;
+        x.right = node;
+
+        x.color = node.color;
+        node.color = RED;
+
+        return x;
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
             size++;
             return new Node(key, value);
         }
-        if (key.compareTo(root.key) < 0) {
-            root.left = put(root.left, key, value);
-        } else if (key.compareTo(root.key) > 0) {
-            root.right = put(root.right, key, value);
+        if (key.compareTo(node.key) < 0) {
+            node.left = put(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = put(node.right, key, value);
         } else {
-            root.value = value;
+            node.value = value;
         }
-        return root;
+
+        if (isRed(node.right) && !isRed(node.left)) {
+            node = leftRotate(node);
+        }
+
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rightRotate(node);
+        }
+
+        if (isRed(node.left) && isRed(node.right)) {
+            flipColors(node);
+        }
+        return node;
     }
 }
